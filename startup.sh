f84121d079b0fea2e9801c02ca7a1ff90436a7a5
David Carboni <david.carboni@ros.gov.uk>
2017-08-23 17:30:08 +0100

    Improved documentation in the startup script.

--- startup.sh
+++ startup.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
+# This script sends a curl request to each of the components
+# in order to "wake" them. Heroku puts apps to sleep when 
+# they are inactive. Attempting to run a distributed trace
+# will result in timeout errors if the apps are asleep.
+
+function startup {
+    echo Starting up ${1}:
+    curl http://${1}.ros.9ov.uk
+    echo
+    echo
+}
+
+echo
 echo "Prompting each component to start up..."
 echo
-curl https://sleuth-d.herokuapp.com
-curl https://sleuth-c.herokuapp.com
-curl https://sleuth-b.herokuapp.com
-curl https://sleuth-a.herokuapp.com
-echo " [finished]"
+
+startup sleuth-d
+startup sleuth-c
+startup sleuth-b
+startup sleuth-a
+
+echo "[finished]"
 echo
 echo "Components are hopefully now started."
-
-echo "To run a distributed trace: curl https://sleuth-a.herokuapp.com"
+echo
+echo "To run a distributed trace:"
+echo "    curl http://sleuth-a.ros.9ov.uk"
+echo
 echo "Trace information will be sent to the log of each application and aggregated to Logentries."
